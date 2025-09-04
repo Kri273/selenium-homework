79fb0f27af0b2059181789cc82cdd1452bf95227
Kri273 <kristelle.tasane@voco.ee>
2025-09-04 13:48:10 +0300

    Lisasin pildid ja vormitäitmisele paar lisa

--- yl1-5/vormitäitmine.py
+++ yl1-5/vormitäitmine.py
@@ -20,5 +20,17 @@ password.send_keys("SuperSecretPassword!")
 login_button = driver.find_element(By.XPATH, "//button[@type='submit']")
 login_button.click()
 
+# kontrolli, kas õnnestus
+try:
+    success_message = driver.find_element(By.ID, "flash").text
+    if "You logged into a secure area!" in success_message:
+        print("TEST ÕNNESTUS")
+    else:
+        print("TEST EBAÕNNESTUS")
+except Exception as e:
+    print("TEST EBAÕNNESTUS")
+    print(str(e))
+
+
 input("Vajuta Enter, et brauser sulgeda...")
 driver.quit()
